974fd2676aa6f370f735f20f03ae6b598ca88dda
Rachel Meyerchick <rachelmeyerchick@Rachels-MacBook-Pro.local>
2019-06-10 17:18:52 -0500

    css responsive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import './Reset.css';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+constructor() {
+  super();
+  this.state = {
+    menuStatus: 'drop-down-menu',
+    buttonStatus: 'drop-down-buttons'
+  }
+}
+
+handleClick = () => {
+  if(this.state.menuStatus === 'drop-down-menu-open') {
+    this.setState({menuStatus: 'drop-down-menu-closed', buttonStatus:'drop-down-buttons-closed'})
+  } else {
+    this.setState({menuStatus: 'drop-down-menu-open', buttonStatus:'drop-down-buttons-open' })
+  }
+}
+
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <nav>
+    <ul>
+        <div>
+        <li id='title'>Start Bootstrap</li>
+        </div>
+        <li className='menu-text'>Home</li>
+        <li className='menu-text'>About</li>
+        <li className='menu-text'>Services</li>
+        <li className='menu-text'>Contact</li>
+        <li>
+          <button 
+          className='menu-button'
+          onClick={this.handleClick} >
+            Menu
+          </button>
+        </li>
+        <div className={this.state.menuStatus}>
+        <li className={this.state.buttonStatus}>Home</li>
+        <li className={this.state.buttonStatus}>About</li>
+        <li className={this.state.buttonStatus}>Services</li>
+        <li className={this.state.buttonStatus}>Contact</li>
+        </div>
+    </ul>
+    {/* <div id='word-container'>
+    <h1 id='welcome'>
+      Welcome To Our Studio!
+    </h1>
+    </div> */}
+  </nav>
+    )
+  }
 }
 
 export default App;
